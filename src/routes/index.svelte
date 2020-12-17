81a62a0f745724a4b022f3e7cff3f7ad195c1bdb
luisgouden <porteiroperez+github@gmail.com>
2020-12-17 17:25:34 +0100

    Estoy loco

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,30 +1,11 @@
+<script>
+let pass='';
+function saveData() {
+	console.log(pass.value)
+
+}
+</script>
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-	figure {
-		margin: 0 0 1em 0;
-	}
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-	p {
-		margin: 1em auto;
-	}
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
 </style>
 
 <svelte:head>
@@ -38,4 +19,7 @@
 	<figcaption>HIGH FIVE!</figcaption>
 </figure>
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<div>
+<input type="text" blind:this="{pass}">
+<button on:click="{saveData}">save</button>
+</div>
