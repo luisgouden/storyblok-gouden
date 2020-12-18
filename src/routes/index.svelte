b4dd6c73df5d5e649f2232a0e652ac9e4d868e20
luisgouden <porteiroperez+github@gmail.com>
2020-12-18 13:23:10 +0100

    en fin

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,25 +1,41 @@
-<script>
-import { goto } from '@sapper/app';
-
-let val='';
-function saveData() {
-	if(val === 'popi'){
-		goto('/portfolio');
-	}else{
-			console.log(val)
-	}
-}
-</script>
 <style>
-
+	h1, figure, p {
+		text-align: center;
+		margin: 0 auto;
+	}
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+	figure {
+		margin: 0 0 1em 0;
+	}
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	}
+	p {
+		margin: 1em auto;
+	}
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
 </style>
 
 <svelte:head>
-	<title>Pipe Prime</title>
+	<title>Sapper project template</title>
 </svelte:head>
 
+<h1>Great success!</h1>
 
-<div>
-<input type="text" bind:value="{val}">
-<button on:click="{saveData}">save</button>
-</div>
+<figure>
+	<img alt='Borat' src='great-success.png'>
+	<figcaption>HIGH FIVE!</figcaption>
+</figure>
+
+<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
